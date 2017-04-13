a5205b4e22c29464960883ab6b01598b2d03a9a4
bennollan <bennollan@gmail.com>
2017-04-12 23:14:37 -0700

    Updated Matrix Multipliers to the new 4x4 affine transform.
    also made some changes to the clamping and added parameters for the
    delay so it would correct itself for bit width changes.

--- Daltonismo/Daltonismo.srcs/sources_1/new/CVD.sv
+++ Daltonismo/Daltonismo.srcs/sources_1/new/CVD.sv
@@ -68,8 +68,28 @@ module CVDCompensation(
     logic [31:0] HSVred;
     logic [31:0] HSVgreen;
     logic [31:0] HSVblue;
+
+    logic[NUM_WID-1:0]inputColorVector[4];
+    assign inputColorVector[0] = {8'b0,redIn,16'b0};
+    assign inputColorVector[1] = {8'b0,greenIn,16'b0};
+    assign inputColorVector[2] = {8'b0,blueIn,16'b0};
+    assign inputColorVector[3] = {8'b0, 8'hFF, 16'b0};
+    logic[PROD_WID-1:0]diffVector[4];
+
+    VectorMatrixMultiplier
+      #(.NUMBER_WIDTH(NUM_WID), .NUMBER_DECIMALS(NUM_DEC),
+      .PRODUCT_WIDTH(PROD_WID), .PRODUCT_DECIMALS(PROD_DEC))
+    Daltonismonster(
+      clk,
+
+      inputColorVector,
+  
+      matrixInOne,
+  
+      diffVector
+      );
     
-    ThreeByThreeMatrixMultiplier
+   /* ThreeByThreeMatrixMultiplier
       #(.NUMBER_WIDTH(NUM_WID), .NUMBER_DECIMALS(NUM_DEC),
       .PRODUCT_WIDTH(PROD_WID), .PRODUCT_DECIMALS(PROD_DEC))
     Daltonismonster(
@@ -90,9 +110,34 @@ module CVDCompensation(
       matrixInOne[8], matrixInOne[9], matrixInOne[10],
   
       diffRed, diffGreen, diffBlue
-      );
+      );*/
     
-    ThreeByThreeMatrixMultiplier
+    logic[NUM_WID-1:0]inputDelayedColorVector[4];
+    assign inputDelayedColorVector[0] = delayedRedIn-diffVector[0];
+    assign inputDelayedColorVector[1] = delayedGreenIn-diffVector[1];
+    assign inputDelayedColorVector[2] = delayedBlueIn-diffVector[2];
+    assign inputDelayedColorVector[3] = 32'h00FF0000;
+    logic[PROD_WID-1:0]outputColorVector[4];
+    assign preCLAMPred = outputColorVector[0];
+    assign preCLAMPgreen = outputColorVector[1];
+    assign preCLAMPblue = outputColorVector[2];
+
+
+    VectorMatrixMultiplier
+      #(.NUMBER_WIDTH(NUM_WID), .NUMBER_DECIMALS(NUM_DEC),
+      .PRODUCT_WIDTH(PROD_WID), .PRODUCT_DECIMALS(PROD_DEC))
+    CorrectionMatrix(
+      clk,
+
+      inputDelayedColorVector,
+  
+      matrixInTwo,
+  
+      outputColorVector
+      );
+
+
+    /*ThreeByThreeMatrixMultiplier
       #(.NUMBER_WIDTH(NUM_WID), .NUMBER_DECIMALS(NUM_DEC),
       .PRODUCT_WIDTH(PROD_WID), .PRODUCT_DECIMALS(PROD_DEC))
     CorrectionMatrix(
@@ -113,23 +158,26 @@ module CVDCompensation(
       matrixInTwo[8], matrixInTwo[9], matrixInTwo[10],
   
       preCLAMPred, preCLAMPgreen, preCLAMPblue
-      );
+      );*/
     
-    DelaySignal #(.DATA_WIDTH(3),.DELAY_CYCLES(75)) SyncDelay(clk,syncIn, syncOut);
+    DelaySignal #(.DATA_WIDTH(3),.DELAY_CYCLES((NUM_WID+2)*2)) SyncDelay(clk,syncIn, syncOut);
   
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) RedDelay(clk,{8'b0,redIn,16'b0}, delayedRedIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) GreenDelay(clk,{8'b0,greenIn,16'b0}, delayedGreenIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) BlueDelay(clk,{8'b0,blueIn,16'b0}, delayedBlueIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) RedDelay(clk,{8'b0,redIn,16'b0}, delayedRedIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) GreenDelay(clk,{8'b0,greenIn,16'b0}, delayedGreenIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) BlueDelay(clk,{8'b0,blueIn,16'b0}, delayedBlueIn);
     
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) RedDelayAGAIN(clk,delayedRedIn, delayedAgainRedIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) GreenDelayAGAIN(clk,delayedGreenIn, delayedAgainGreenIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) BlueDelayAGAIN(clk,delayedBlueIn, delayedAgainBlueIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) RedDelayAGAIN(clk,delayedRedIn, delayedAgainRedIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) GreenDelayAGAIN(clk,delayedGreenIn, delayedAgainGreenIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) BlueDelayAGAIN(clk,delayedBlueIn, delayedAgainBlueIn);
     
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(36)) RedDiffDelay(clk,diffRed, delayedDiffRed);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(36)) GreenDiffDelay(clk,diffGreen, delayedDiffGreen);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(36)) BlueDiffDelay(clk,diffBlue, delayedDiffBlue);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) RedDiffDelay(clk,diffRed, delayedDiffRed);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) GreenDiffDelay(clk,diffGreen, delayedDiffGreen);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) BlueDiffDelay(clk,diffBlue, delayedDiffBlue);
     
-    always_ff @(posedge clk)
+
+
+
+    /*always_ff @(posedge clk)
       begin
       
         if(enable)
@@ -204,7 +252,46 @@ module CVDCompensation(
         
         
         
+      end*/
+    function logic[7:0] TheCLAMP(logic[31:0] notClamped);
+      logic[7:0] clampidia;
+        if(notClamped[31])
+          clampidia = 0;
+        else if(notClamped[31:16] > 255)
+          clampidia = 255;
+        else
+          clampidia = notClamped[23:16];
+  
+      return clampidia;
+    
+    endfunction : TheCLAMP
+
+
+    always_comb
+      begin
+        logic [31:0] CLAMPred;
+        logic [31:0] CLAMPgreen;
+        logic [31:0] CLAMPblue;
+
+        if(enable)
+          begin
+            CLAMPred = preCLAMPred + delayedAgainRedIn;
+            CLAMPgreen = preCLAMPgreen + delayedAgainGreenIn;
+            CLAMPblue = preCLAMPblue + delayedAgainBlueIn;
+          end
+        else
+          begin
+            CLAMPred = delayedAgainRedIn;
+            CLAMPgreen = delayedAgainGreenIn;
+            CLAMPblue = delayedAgainBlueIn;
+          end
+            
+        redOut = TheCLAMP(CLAMPred);
+        greenOut = TheCLAMP(CLAMPgreen);
+        blueOut = TheCLAMP(CLAMPblue); 
       end
+
+
 endmodule
 
 
@@ -231,118 +318,79 @@ module CVDSimulation(
     logic [31:0] delayedRedIn;
     logic [31:0] delayedGreenIn;
     logic [31:0] delayedBlueIn;
-    
-    logic [31:0] CLAMPred;
-    logic [31:0] CLAMPgreen;
-    logic [31:0] CLAMPblue;
-  
+      
     logic [31:0] preCLAMPred;
     logic [31:0] preCLAMPgreen;
     logic [31:0] preCLAMPblue;
     
-    logic [31:0] HSVred;
-    logic [31:0] HSVgreen;
-    logic [31:0] HSVblue;
-    
-    DelaySignal #(.DATA_WIDTH(3),.DELAY_CYCLES(35)) SyncDelay(clk,syncIn, syncOut);
+    DelaySignal #(.DATA_WIDTH(3),.DELAY_CYCLES(NUM_WID+2)) SyncDelay(clk,syncIn, syncOut);
   
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) RedDelay(clk,{8'b0,redIn,16'b0}, delayedRedIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) GreenDelay(clk,{8'b0,greenIn,16'b0}, delayedGreenIn);
-    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(35)) BlueDelay(clk,{8'b0,blueIn,16'b0}, delayedBlueIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) RedDelay(clk,{8'b0,redIn,16'b0}, delayedRedIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) GreenDelay(clk,{8'b0,greenIn,16'b0}, delayedGreenIn);
+    DelaySignal #(.DATA_WIDTH(32),.DELAY_CYCLES(NUM_WID+2)) BlueDelay(clk,{8'b0,blueIn,16'b0}, delayedBlueIn);
     
     
-    ThreeByThreeMatrixMultiplier
+    logic[NUM_WID-1:0]inputColorVector[4];
+    assign inputColorVector[0] = {8'b0,redIn,16'b0};
+    assign inputColorVector[1] = {8'b0,greenIn,16'b0};
+    assign inputColorVector[2] = {8'b0,blueIn,16'b0};
+    assign inputColorVector[3] = {8'b0, 8'hFF, 16'b0};
+    logic[PROD_WID-1:0]outputColorVector[4];
+    assign preCLAMPred = outputColorVector[0];
+    assign preCLAMPgreen = outputColorVector[1];
+    assign preCLAMPblue = outputColorVector[2];
+
+
+    VectorMatrixMultiplier
       #(.NUMBER_WIDTH(NUM_WID), .NUMBER_DECIMALS(NUM_DEC),
       .PRODUCT_WIDTH(PROD_WID), .PRODUCT_DECIMALS(PROD_DEC))
-    CorrectionMatrix(
+    SimulationMatrix(
       clk,
-      
-      {8'b0,redIn,16'b0},
-      {8'b0,greenIn,16'b0},
-      {8'b0,blueIn,16'b0},
+
+      inputColorVector,
   
-      matrixIn[0], matrixIn[1], matrixIn[2],
-      matrixIn[4], matrixIn[5], matrixIn[6],
-      matrixIn[8], matrixIn[9], matrixIn[10],
+      matrixIn,
   
-      preCLAMPred, preCLAMPgreen, preCLAMPblue
+      outputColorVector
       );
+
+
+    function logic[7:0] TheCLAMP(logic[31:0] notClamped);
+      logic[7:0] clampidia;
+        if(notClamped[31])
+          clampidia = 0;
+        else if(notClamped[31:16] > 255)
+          clampidia = 255;
+        else
+          clampidia = notClamped[23:16];
+  
+      return clampidia;
     
-    always_ff @(posedge clk)
+    endfunction : TheCLAMP
+
+
+    always_comb
       begin
-      
+        logic [31:0] CLAMPred;
+        logic [31:0] CLAMPgreen;
+        logic [31:0] CLAMPblue;
+
         if(enable)
           begin
-            CLAMPred <= preCLAMPred;
-            CLAMPgreen <= preCLAMPgreen;
-            CLAMPblue <= preCLAMPblue;
+            CLAMPred = preCLAMPred;
+            CLAMPgreen = preCLAMPgreen;
+            CLAMPblue = preCLAMPblue;
           end
         else
           begin
-            CLAMPred <= delayedRedIn;
-            CLAMPgreen <= delayedGreenIn;
-            CLAMPblue <= delayedBlueIn;
+            CLAMPred = delayedRedIn;
+            CLAMPgreen = delayedGreenIn;
+            CLAMPblue = delayedBlueIn;
           end
-        
-        ////CLAMP RED
-        //If the red number is beigger than 255
-        if(CLAMPred[31] != 1'b1 && CLAMPred[31:16] > 255)
-            begin
-                HSVred[23:16] <= 255;
-            end
-        //If the red number is negative
-        else if(CLAMPred[31])
-            begin
-                HSVred[23:16] <= 0;
-            end
-        //Otherwise
-        else
-            begin
-                HSVred <= CLAMPred;
-            end
             
-        ////CLAMP GREEN
-        //If the green number is beigger than 255
-        if(CLAMPgreen[31] != 1'b1 && CLAMPgreen[31:16] > 255)
-            begin
-                HSVgreen[23:16] <= 255;
-            end
-        //If the green number is negative
-        else if(CLAMPgreen[31])
-            begin
-                HSVgreen[23:16] <= 0;
-            end
-        //Otherwise
-        else
-            begin
-                HSVgreen <= CLAMPgreen;
-            end
-            
-        ////CLAMP BLUE
-        //If the blue number is beigger than 255
-        if(CLAMPblue[31] != 1'b1 && CLAMPblue[31:16] > 255)
-            begin
-                HSVblue[23:16] <= 255;
-            end
-        //If the blue number is negative
-        else if(CLAMPblue[31])
-            begin
-                HSVblue[23:16] <= 0;
-            end
-        //Otherwise
-        else
-            begin
-                HSVblue <= CLAMPblue;
-            end
-            
-            
-            
-        redOut <= HSVred[23:16];
-        greenOut <= HSVgreen[23:16];
-        blueOut <= HSVblue[23:16];
-        
-        
-        
+        redOut = TheCLAMP(CLAMPred);
+        greenOut = TheCLAMP(CLAMPgreen);
+        blueOut = TheCLAMP(CLAMPblue); 
       end
     
 endmodule
